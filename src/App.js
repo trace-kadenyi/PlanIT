90a2ce1f897ebae5345ccdb135d643c55a742ca0
Tracey Kadenyi <treykadenyi@gmail.com>
2024-06-26 16:16:03 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
     </div>
   );
-}
+};
 
 export default App;
